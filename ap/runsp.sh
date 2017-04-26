95e86d085d8e451858ad4445d7261dac033d7498
Jesus <jesus@trantor>
2017-04-26 14:48:46 -0500

    Terminando proceso antes de ejecutarlo 	modified:   ap/runsp.sh

--- ap/runsp.sh
+++ ap/runsp.sh
@@ -1,7 +1,9 @@
+kill $(pidof hostapd)
 hostapd -B /etc/sink-ap/hostapd.conf
 
 ip addr flush wlan0
 ip addr add 192.168.1.1/24 broadcast 192.168.1.0 dev wlan0
 ip link set wlan0 up
 
+kill $(pidof dhcpd)
 dhcpd -q -d -cf /etc/sink-ap/dhcpd.conf wlan0
