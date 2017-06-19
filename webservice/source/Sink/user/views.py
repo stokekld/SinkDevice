0706fc69bcba005869b1d85d57f324db63145f98
Jesus Flores <joy_warmgun@hotmail.com>
2017-06-19 18:16:16 -0500

    Canbiando salida de vista 	modified:   webservice/source/Sink/user/views.py

--- webservice/source/Sink/user/views.py
+++ webservice/source/Sink/user/views.py
@@ -1,10 +1,11 @@
-from flask import Response
+from flask import redirect, url_for, request
 from Sink.app import service
 
 from .models import db
 
 @service.route('/auth', methods=['POST'])
 def principal():
-    device = db.device.find_one()
-    print device
-    return Response()
+    # device = db.device.find_one()
+    # print device
+    print request.form
+    return redirect(url_for('index'))
