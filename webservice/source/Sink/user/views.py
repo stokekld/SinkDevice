068d92e27b0efeb2021e0f1237499d11fddeb673
Jesus Flores <joy_warmgun@hotmail.com>
2017-06-20 14:47:59 -0500

    modified:   webservice/source/Sink/user/views.py

--- webservice/source/Sink/user/views.py
+++ webservice/source/Sink/user/views.py
@@ -1,11 +1,15 @@
 from flask import redirect, url_for, request
 from Sink.app import service
+import md5
 
 from .models import db
 
 @service.route('/auth', methods=['POST'])
-def principal():
-    # device = db.device.find_one()
-    # print device
-    print request.form
-    return redirect(url_for('index'))
+def authorization():
+    credentials = request.form
+    device = db.device.find_one()
+
+    if device["user"] == credentials["user"] and device["password"] == md5.new(credentials["password"]).hexdigest():
+        return redirect(url_for('principal'))
+    else:
+        return redirect(url_for('index'))
